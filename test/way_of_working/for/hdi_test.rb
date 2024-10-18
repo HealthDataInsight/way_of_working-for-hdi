555e4625de11fd7eeb2bb8fa47e8719cd0937b87
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-10-18 09:28:02 +0100

    Linting

--- test/way_of_working/for/hdi_test.rb
+++ test/way_of_working/for/hdi_test.rb
@@ -12,13 +12,13 @@ module WayOfWorking
       def test_sub_command_lists_exit_with_status_zero
         _output = `bundle exec way_of_working exec`
         assert $CHILD_STATUS.success?
-  
+
         _output = `bundle exec way_of_working init`
         assert $CHILD_STATUS.success?
-  
+
         _output = `bundle exec way_of_working new`
         assert $CHILD_STATUS.success?
       end
-      end
+    end
   end
 end
