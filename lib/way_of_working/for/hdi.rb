962a317cf9d3010b752280975974901a87dcadbc
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-10-18 17:49:28 +0100

    Moved rake_tasks here

--- lib/way_of_working/for/hdi.rb
+++ lib/way_of_working/for/hdi.rb
@@ -6,6 +6,9 @@ require 'zeitwerk'
 loader = Zeitwerk::Loader.for_gem_extension(WayOfWorking::For)
 loader.setup
 
+# Require "internal" plugin(s) here
+require 'way_of_working/rake_tasks'
+
 module WayOfWorking
   module For
     module Hdi
