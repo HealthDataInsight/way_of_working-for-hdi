812583d2afa07fa41d05c240e53b1dd878483453
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-11-04 16:52:30 +0000

    Added init command

--- test/way_of_working/for/hdi_test.rb
+++ test/way_of_working/for/hdi_test.rb
@@ -8,6 +8,17 @@ module WayOfWorking
       def test_that_it_has_a_version_number
         refute_nil ::WayOfWorking::For::Hdi::VERSION
       end
+
+      def test_sub_command_lists_exit_with_status_zero
+        result = system('bundle exec way_of_working exec > /dev/null 2>&1')
+        assert result, "way_of_working exec command failed"
+
+        result = system('bundle exec way_of_working init > /dev/null 2>&1')
+        assert result, "way_of_working init command failed"
+
+        result = system('bundle exec way_of_working new > /dev/null 2>&1')
+        assert result, "way_of_working new command failed"
+      end
     end
   end
 end
