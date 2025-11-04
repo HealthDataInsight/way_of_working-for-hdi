5dda6016433b1547235604520a8f4feefece8a34
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-11-04 17:04:17 +0000

    Linting

--- test/way_of_working/for/hdi_test.rb
+++ test/way_of_working/for/hdi_test.rb
@@ -11,13 +11,13 @@ module WayOfWorking
 
       def test_sub_command_lists_exit_with_status_zero
         result = system('bundle exec way_of_working exec > /dev/null 2>&1')
-        assert result, "way_of_working exec command failed"
+        assert result, 'way_of_working exec command failed'
 
         result = system('bundle exec way_of_working init > /dev/null 2>&1')
-        assert result, "way_of_working init command failed"
+        assert result, 'way_of_working init command failed'
 
         result = system('bundle exec way_of_working new > /dev/null 2>&1')
-        assert result, "way_of_working new command failed"
+        assert result, 'way_of_working new command failed'
       end
     end
   end
