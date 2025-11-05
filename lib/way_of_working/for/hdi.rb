e8094ac26f47f166fae4c7179c0fa2a9acb517bf
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-11-05 13:30:56 +0000

    Way of Working rake tasks initialiser (#30)

--- lib/way_of_working/for/hdi.rb
+++ lib/way_of_working/for/hdi.rb
@@ -7,37 +7,13 @@ require 'zeitwerk'
 loader = Zeitwerk::Loader.for_gem_extension(WayOfWorking::For)
 loader.setup
 
+# Require "internal" plugin(s) here
+require 'way_of_working/rake_tasks'
+
 module WayOfWorking
   module For
     module Hdi
       class Error < StandardError; end
     end
-  end
-
-  module SubCommands
-    # # This reopens the "way_of_working audit" sub command
-    # class Audit
-    #   register(For::Hdi::Generators::Audit, 'for', 'for',
-    # end
-    
-    # # This reopens the "way_of_working document" sub command
-    # class Document
-    #   register(For::Hdi::Generators::Document, 'for', 'for',
-    # end
-    
-    # # This reopens the "way_of_working exec" sub command
-    # class Exec
-    #   register(For::Hdi::Generators::Exec, 'for', 'for',
-    # end
-
-    # # This reopens the "way_of_working init" sub command
-    # class Init
-    #   register(For::Hdi::Generators::Init, 'for', 'for',
-    # end
-
-    # # This reopens the "way_of_working new" sub command
-    # class New
-    #   register(For::Hdi::Generators::New, 'for', 'for [NAME]',
-    # end
   end
 end
